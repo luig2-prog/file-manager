6cb4161bac50fb87802216f0fc24ae228a649958
Luis Manuel Hernandez Jimenez <luis.hernandez@grupokonecta.com>
2023-10-08 19:48:45 -0500

    ✨ add useEfect

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,28 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ButtonComponent from "./ButtonComponent";
 
 const Counter = () => {
-    const [counter, setCounter] = useState(0);
-    return (
-        <>
-            <div>
-                Numero en: {counter}
-            </div>
-            <ButtonComponent text="Aumentar" functionComponent={() => {
-                setCounter(counter + 1);
-            }}/>
-            <ButtonComponent functionComponent={() => {
-                setCounter(counter - 1);
-            }}>
-                Disminuir
-            </ButtonComponent>
-            <ButtonComponent functionComponent={() => {
-                setCounter(0);
-            }}>
-                Reiniciar
-            </ButtonComponent>
-        </>
-    )
-}
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("render anywhay");
+  });
+
+  useEffect(() => {
+    console.log("render one way");
+  }, []);
+
+  useEffect(() => {
+    console.log("render with dependencies with counter");
+  }, [counter]);
+
+  return (
+    <>
+      <div>Numero en: {counter}</div>
+      <ButtonComponent
+        text="Aumentar"
+        functionComponent={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <ButtonComponent
+        functionComponent={() => {
+          setCounter(counter - 1);
+        }}
+      >
+        Disminuir
+      </ButtonComponent>
+      <ButtonComponent
+        functionComponent={() => {
+          setCounter(0);
+        }}
+      >
+        Reiniciar
+      </ButtonComponent>
+    </>
+  );
+};
 
 export default Counter;

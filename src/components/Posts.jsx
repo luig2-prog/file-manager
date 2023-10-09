73bf89ab4576f654ee813d1b046813a7127d5167
Luis Manuel Hernandez Jimenez <luis.hernandez@grupokonecta.com>
2023-10-08 20:58:55 -0500

    ✨ adding posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,27 +1,25 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ButtonComponent from "./ButtonComponent";
 import { RiRadioButtonLine } from "react-icons/ri";
 
 const PostsComponent = () => {
   const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    console.log("🚀 ~ file: Posts.jsx:7 ~ PostsComponent ~ posts:", posts);
-  }, [posts]);
   const onSubmit = async () => {
     const fetchData = await fetch("https://jsonplaceholder.typicode.com/posts");
     const jsonData = await fetchData.json();
-    console.log("🚀 ~ file: Posts.jsx:11 ~ onSubmit ~ jsonData:", jsonData);
     setPosts(jsonData);
   };
 
   return (
     <div>
-      {posts.map((post) => {
-        <div key={post.id}>
-          Title: <div>{post.title}</div>
-        </div>;
-      })}
+      <div className="grid grid-cols-12">
+        {posts.map((post) => (
+          <div className="bg-[#1266f1] m-2 text-white" key={post.id}>
+            Title: <div>{post.title}</div>
+          </div>
+        ))}
+      </div>
       <ButtonComponent text="Fetch Data" functionComponent={onSubmit}>
         <RiRadioButtonLine />
       </ButtonComponent>
